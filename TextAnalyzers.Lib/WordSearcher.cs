afbe71fbf87674fa41797517ed47b0b42a4a700b
Mark Rosenberg <mrosenberg@prscholas.org>
2021-04-09 12:33:10 -0500

    Add project files.

---
+++ TextAnalyzers.Lib/WordSearcher.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAnalyzers.Lib
+{
+    public class WordSearcher
+    {
+        public WordSearcher(string _document)
+        {
+            if (string.IsNullOrEmpty(_document))
+            {
+                throw new ArgumentNullException(nameof(_document));
+            }
+            Document = _document;
+        }
+        public string Document { get; private set; }
+
+        public int GetWordCount(string searchText, bool isCaseSensitive = true)
+        {
+            StringComparison scomp = isCaseSensitive ? StringComparison.InvariantCulture :
+                StringComparison.InvariantCultureIgnoreCase;
+            int count = 0;
+            int ndx =Document.IndexOf(searchText, scomp);
+            while (ndx >= 0)
+            {
+                count++;
+                ndx = Document.IndexOf(searchText, ndx + searchText.Length, scomp);
+            }
+            return count;
+        }
+        public static WordSearcher FromFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new ArgumentException($"File not found: {filePath}");
+            }
+            return new WordSearcher(File.ReadAllText(filePath));
+        }
+    }
+}
---
+++ WordCounterConsole/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextAnalyzers.Lib;
+
+namespace WordCounterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("1 argument expected");
+                return;
+            }
+            WordSearcher searcher = WordSearcher.FromFile(args[0]);
+            do
+            {
+                string input = PromptForSearchWord("Please enter text to search for: ");
+            AskCase:
+
+                string caseSen = PromptForSearchWord("Should this search be case sensitive? y/n ");
+                bool cs = false;
+                switch (caseSen.ToLower())
+                {
+                    case "y":
+                        cs = true;
+                        break;
+                    case "n":
+                        cs = false;
+                        break;
+                    default:
+                        Console.WriteLine("You must enter Y or N");
+                        goto AskCase;
+                        break;
+                }
+                if (string.IsNullOrWhiteSpace(input)) return;
+                int nOccur = searcher.GetWordCount(input, cs);
+                Console.WriteLine($"The text {input} appears {nOccur} times");
+
+            } while (true);
+        }
+
+        private static string PromptForSearchWord(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }  
+    }
+}
